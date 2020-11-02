3f45ace19da285be87edea8c9fd64096b3db9bc4
Dasa Riadi <DASA.RIADI97@GMAIL.COM>
2020-11-02 11:40:55 -0500

    final commit

--- todo-app/src/app/app.component.ts
+++ todo-app/src/app/app.component.ts
@@ -29,4 +29,16 @@ export class AppComponent {
     priority: 5
     }
   ];
+
+  addTodo(newTodoLabel){
+    var newTodo = {
+      label: newTodoLabel,
+      priority: 1,
+      done: false
+    };
+    this.todos.push(newTodo)
+  }
+  deleteTodo(todo){
+    this.todos = this.todos.filter( t => t.label !== todo.label );
+  }
 }
